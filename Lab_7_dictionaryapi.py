0d0774150facc02052a0da06c942a14dd225ec96
egrox <negor3893@gmail.com>
2026-02-22 17:49:23 +0300

    final

---
+++ Lab_7_dictionaryapi.py
@@ -0,0 +1,76 @@
+import requests
+import json
+
+
+def word_info(data):
+    #слово не найдено 
+    if isinstance(data[0], str): #проверка первой строки на тип
+        print("слово не найдено")
+        return 
+
+    #основное значение
+    entry = data[0]
+
+    #cлово
+    print("№1")
+    print(f"Слово: {entry["hwi"]["hw"].replace("*", "")}", "\n")
+    
+    #произношение 
+    print("№2")
+    print(f"Произношение: {entry["hwi"]["prs"][0]["ipa"] }", "\n")
+
+    #часть речи
+    print("№3")
+    print(f"Часть речи: {entry["fl"]}", "\n")
+
+    #определения
+    print("№4")
+    shortdef = entry["shortdef"]
+    if shortdef:
+        print("Определения:")
+        i = 1
+        for text in (shortdef):
+            print(f"  {i}) {text}", "\n")
+            i += 1
+    else:
+        print("Определений нет", "\n")
+
+    #формы слова
+    print("№5")
+    stems = entry["meta"]["stems"]
+    if stems:
+        print(f"Другие формы: {", ".join(stems)}")
+    print()
+
+    #аудио
+    print("№6")
+    print(f"Аудио: файл {entry["hwi"]["prs"][0]["sound"]["audio"]}", "\n")
+
+    #другие формы слова
+    print("№7")
+    stems = entry["meta"]["stems"]
+    if stems:
+        print(f"Другие формы: {", ".join(stems)}")
+    else:
+        print("Форм нет", "\n")
+
+
+def fetch_learners_dictionary(word):
+    base_url = \
+    f"https://www.dictionaryapi.com/api/v3/references/learners/json/{word}"
+    
+    #https://dictionaryapi.com/products/api-learners-dictionary
+    params = {
+        'key': "e1122957-c9cf-4f1f-8f6f-a4c039d5b6f5"
+    }
+
+    try:
+        response = requests.get(base_url, params=params)
+        data = response.json()
+        return word_info(data)
+        
+    except:
+        print(response.status_code)
+
+search_word = "summer"
+dictionary_data = fetch_learners_dictionary(search_word)
--- Lab_7_weather.py
+++ Lab_7_weather.py
@@ -1,37 +1,37 @@
 import requests
-    
+
 
 def get_weather(city_name):
     base_url = "http://api.openweathermap.org/data/2.5/weather"
     
-    params = {
+    #https://openweathermap.org/api/one-call-3?collection=one_call_api_3.0
+    params = { 
         'q': city_name,
         'appid': "27503d8d30e83df5f1f8719adb9afa0c", #API
-        'units': 'metric',  # градусы по ццельсию
-        'lang': 'ru'        # вывод погоды на русском
+        'units': 'metric',  # градусы по цельсию
+        'lang': 'ru' # вывод погоды на русском
     }
-    
+
     try:
-        response = requests.get(base_url, params=params)        
-        # Парсим JSON-ответ
-        data = response.json()
-        
-        # Извлекаем нужные данные
+        response = requests.get(base_url, params=params) #отправляем запрос       
+        data = response.json() #парсим JSON
+
+        #находим данные
+        weather_desc = data['weather'][0]['description']
         temp = data['main']['temp']
         humidity = data['main']['humidity']
-        pressure = data['main']['pressure']  # в гектопаскалях (гПа)
-        weather_desc = data['weather'][0]['description']
-        
-        # Выводим результат
+        pressure = data['main']['pressure']  # в гектопаскалях
+
         print(f"Погода в городе {city_name}:")
         print(f"Описание: {weather_desc.capitalize()}")
         print(f"Температура: {temp}°C")
         print(f"Влажность: {humidity}%")
         print(f"Давление: {pressure} гПа")
-        
-    except:
+
+    except: 
         print(response.status_code)
 
-if __name__ == "__main__":
-    city = "Saint"
-    get_weather(city)
+
+city = "Saint Petersburg"
+city = "Ulianovsk"
+get_weather(city)
